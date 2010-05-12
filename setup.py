557127ff2760e50f444953ac252ca01b031ddb4f
Ted Kaemming <ted@kaemming.com>
2010-05-12 15:01:21 -0500

    added process_response method to middleware that forces the "Vary" header to avoid caching across subdomains
    thanks to john boxall (http://twitter.com/johnboxall) for the tip!

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-subdomains',
-    version = '1.0.0',
+    version = '1.0.1',
     url = 'http://github.com/tkaemming/django-subdomains/',
     packages = find_packages('src'),
     package_dir = {'': 'src'},
--- src/subdomains/middleware.py
+++ src/subdomains/middleware.py
@@ -1,4 +1,5 @@
 import re
+from django.utils.cache import patch_vary_headers
 from django.conf import settings
 from django.contrib.sites.models import Site
 from subdomains.exceptions import IncorrectSiteException
@@ -43,3 +44,16 @@ class SubdomainURLRoutingMiddleware(SubdomainMiddleware):
         
         # Continue processing the request as normal.
         return None
+    
+    def process_response(self, request, response):
+        """
+        Forces the HTTP Vary header onto requests to avoid having responses
+        cached from incorrect urlconfs.
+        
+        If you'd like to disable this for some reason, set `FORCE_VARY_ON_HOST`
+        in your Django settings file to `False`.
+        """
+        if getattr(settings, 'FORCE_VARY_ON_HOST', True):
+            patch_vary_headers(response, ('Host',))
+        
+        return response
