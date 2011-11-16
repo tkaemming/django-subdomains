a652c0ae39f3ea15c4022cc977d9af7a5330a5bb
Ted Kaemming <ted@kaemming.com>
2011-11-15 16:36:52 -0800

    setup.py changes for pypi

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-subdomains',
-    version = '1.1.1',
+    version = '1.1.2',
     url = 'http://github.com/tkaemming/django-subdomains/',
+    author = 'ted kaemming',
+    author_email = 'ted@kaemming.com',
+    description = "Subdomain tools for the Django framework, including "
+        "subdomain-specific URL routing.",
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     include_package_data = True,
