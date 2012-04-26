4ab48cc77389adb23ef71442a4249b3c62fa5c98
Ted Kaemming <ted@kaemming.com>
2012-04-26 12:47:14 -0700

    subdomains/settings.py is no longer necessary

--- subdomains/settings.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-
-
-USE_SUBDOMAIN_EXCEPTION = getattr(settings, 'USE_SUBDOMAIN_EXCEPTION', False)
-
-REMOVE_WWW_FROM_DOMAIN = getattr(settings, 'REMOVE_WWW_FROM_DOMAIN', False)
