1d19b907a1414ce541d547f12f110018897f3f9b
Ted Kaemming <ted@kaemming.com>
2012-09-10 23:24:07 -0700

    Version bump to 1.2.2

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
+import os, sys
+
 from setuptools import setup
+
+
+def get_version():
+    from subdomains import __version__
+    return '.'.join(map(str, __version__))
+
+try:
+    version = get_version()
+except ImportError:
+    sys.path.append(os.path.join(os.path.dirname(__file__, 'subdomains')))
+    version = get_version()
+
 
 install_requires = ['django']
 
 setup(name='django-subdomains',
-    version='1.2.1',
+    version=version,
     url='http://github.com/tkaemming/django-subdomains/',
     author='ted kaemming',
     author_email='ted@kaemming.com',
