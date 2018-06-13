a0dd90b7b6e20a1d9886235926e34ae3a1f81efb
Aykol Koykelov <kolyakoikelov@gmail.com>
2018-06-13 17:11:17 +0600

    * fix bug in utils.py: import django.urls for django < 1.10

--- subdomains/utils.py
+++ subdomains/utils.py
@@ -5,7 +5,10 @@ except ImportError:
     from urllib.parse import urlunparse
 
 from django.conf import settings
-from django.core.urlresolvers import reverse as simple_reverse
+try:
+    from django.core.urlresolvers import reverse as simple_reverse
+except ImportError:
+    from django.urls import reverse as simple_reverse
 
 
 def current_site_domain():
