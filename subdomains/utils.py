cbdc068fd9d506e74f91f7add04a7adf7929d37c
Qiushi Pan <qiu.gits@gmail.com>
2018-06-30 11:11:42 +0900

    Fix error by django.core.urlresolvers
    Fix dependency error in Django >= 1.9

--- subdomains/utils.py
+++ subdomains/utils.py
@@ -5,7 +5,10 @@ except ImportError:
     from urllib.parse import urlunparse
 
 from django.conf import settings
-from django.core.urlresolvers import reverse as simple_reverse
+try:
+    from django.core.urlresolvers import reverse as simple_reverse
+except:
+    from django.urls import reverse as simple_reverse
 
 
 def current_site_domain():
